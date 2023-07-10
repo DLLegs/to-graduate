b9381d0f35b7b0fb82d1d3e8eb8f90bfb5349048
DLongLegs <xoals73771@gmail.com>
2023-07-11 01:11:00 +0900

    Create day7.py

---
+++ string/day7.py
@@ -0,0 +1,13 @@
+# 사용자 입출력
+# input 사용하기
+# input("안내_문구")
+a = input("숫자를 입력하세요: ")
+print(a)
+# input은 입력되는 모든 것을 문자열로 취급
+
+print("life" "is" "too short")
+print("life" + "is" + "too short")
+# 두 문장의 결과값은 같다
+
+print("life","is","too short")
+# 띄워쓰기를 할때는 , 이용
