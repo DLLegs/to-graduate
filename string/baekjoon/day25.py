65fb6f91bbbfec6c54fd4eb8d39e7361e25e156f
DLongLegs <xoals73771@gmail.com>
2024-01-02 14:05:26 +0900

    Create day25.py

---
+++ string/baekjoon/day25.py
@@ -0,0 +1,30 @@
+# 백준 5355번 문제
+# 입려값: 테스트 케이스의 개수, 입력값
+# 출력값: 계산결과
+
+# many = int(input())
+# for i in range(many):
+#     line = list(map(str, input().split()))
+#     ans = line[0]
+#     for j in range(len(line)):
+#         if j == 0:
+#             ans = float(line[j])
+#         elif line[j] == '@':
+#             ans *= 3
+#         elif line[j] == '%':
+#             ans += 5
+#         elif line[j] == '#':
+#             ans -= 7
+#     print("{:.2f}".format(ans))
+
+# 백준 2935번 문제
+# 입력값: 양의 정수, 연산자(+,*), 양의 정수
+# 출력값: 연산 결과
+
+# A = int(input())
+# t = input()
+# B = int(input())
+# if t == '*':
+#     print(A*B)
+# elif t == '+':
+#     print(A+B)
