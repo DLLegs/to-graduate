1df16fe49526da47bea1db3b3b9b088bf8cc4d4b
DLongLegs <xoals73771@gmail.com>
2024-01-31 16:36:00 +0900

    Update day32.py

--- string/baekjoon/day32.py
+++ string/baekjoon/day32.py
@@ -31,17 +31,40 @@
 #     C = (time%60)//10
 #     print(A, B, C)
 
-# 백준 10103번 문제
+# # 백준 10103번 문제
 
-round = int(input())
-Chang, Syang = 100, 100
+# round = int(input())
+# Chang, Syang = 100, 100
 
-for _ in range(round):
-    C_dice, S_dice = map(int, input().split())
-    if C_dice > S_dice:
-        Syang -= C_dice
-    elif S_dice > C_dice:
-        Chang -= S_dice
+# for _ in range(round):
+#     C_dice, S_dice = map(int, input().split())
+#     if C_dice > S_dice:
+#         Syang -= C_dice
+#     elif S_dice > C_dice:
+#         Chang -= S_dice
 
-print(Chang)
-print(Syang)
+# print(Chang)
+# print(Syang)
+
+# # 벡준 10214번 문제
+
+# case = int(input())
+
+# for _ in range(case):
+#     Y = 0
+#     K = 0
+    
+#     for _ in range(9):
+#         y, k = map(int, input().split())
+#         Y += y
+#         K += k
+
+#     if Y > K:
+#         print("Yonsei")
+#     elif Y < K:
+#         print("Korea")
+#     else:
+#         print("Draw")
+
+## 백준 11557번 문제
+
