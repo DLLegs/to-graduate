a28cf2676cd8391cf9bc732440059204ed05f4f2
DLongLegs <xoals73771@gmail.com>
2024-05-27 14:35:31 +0900

    Update test.py

--- string/test.py
+++ string/test.py
@@ -1,12 +1,5 @@
-hour, min = map(int,input().split())
-plus = int(input())
+import matplotlib.pyplot as plt
 
-if min + plus >=60:
-    Rmin = (min + plus) % 60
-    hour = hour + ((min + plus) // 60)
-else:
-    min = min + plus
-if hour >= 24:
-    hour = hour - 24
-
-print(hour, Rmin)
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
