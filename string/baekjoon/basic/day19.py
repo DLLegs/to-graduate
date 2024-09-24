7f6e40e9f9b0f6b1166aa2a8d480cda8a9d1230e
DLongLegs <100695795+DLLegs@users.noreply.github.com>
2024-09-24 22:23:38 +0900

    Update day19.py

--- string/baekjoon/basic/day19.py
+++ string/baekjoon/basic/day19.py
@@ -2,11 +2,11 @@
 # 입력값: 문자열
 # 출력값: 입력값 그대로
 
-# while True:
-#     try:
-#         print(input())
-#     except EOFError:
-#         break
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
 
 # 백준 25083번 문제
 # 입력값: 없음
@@ -23,21 +23,21 @@
 # 입력값: 찾은 체스말의 개수
 # 출력값: 정확한 피스 수
 
-# akf = [1,1,2,2,2,8]
-# user_input = list(map(int,input().split()))
+akf = [1,1,2,2,2,8]
+user_input = list(map(int,input().split()))
 
-# for i in range(len(akf)):
-#     print(akf[i] - user_input[i], end=' ') #end = '' 줄바꿈 안되게함
+for i in range(len(akf)):
+    print(akf[i] - user_input[i], end=' ') #end = '' 줄바꿈 안되게함
 
 # 백준 2444번 문제
 # 입력값: 숫자
 # 출력값: 1부터 줄바꿈 입력 숫자까지
 
-# a = int(input())
-# for i in range(1, a+1):
-#     print(" "*(a-i) + "*"*(2*i-1))
-# for i in range(a-1,0,-1):
-#     print(" "*(a-i) + "*"*(2*i-1))
+a = int(input())
+for i in range(1, a+1):
+    print(" "*(a-i) + "*"*(2*i-1))
+for i in range(a-1,0,-1):
+    print(" "*(a-i) + "*"*(2*i-1))
 
 # 백준 10988번 문제
 # 입력값: 영어 문자열
