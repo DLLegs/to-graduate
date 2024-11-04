4604ec01faab6e22edb8447a0b4f19f11ac82b8d
DLongLegs <xoals73771@gmail.com>
2024-11-04 22:19:48 +0900

    Create MenOfPassion.py

---
+++ string/baekjoon/shot_time/MenOfPassion.py
@@ -0,0 +1,16 @@
+##MenOfPassion(A[],n){
+#     sum < - 0;
+#     for i < - 1 to n
+#         for j < -1 to n
+#             sum < - sum + A[i] * A[j];
+#     return sum;
+# }
+
+# 시간 복잡도 문제
+N = int(input())
+print(N**2)
+print(2)
+
+n = int(input())
+print(int(n*(n-1) - (n-1)*n/2))
+print(2)
