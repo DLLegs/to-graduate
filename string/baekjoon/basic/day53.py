cf5d899dd521ae30d67d2b65b867793aa6035734
DLongLegs <xoals73771@gmail.com>
2024-11-20 13:14:38 +0900

    Update day53.py

--- string/baekjoon/basic/day53.py
+++ string/baekjoon/basic/day53.py
@@ -1,13 +1,15 @@
 # 백준 1978번 문제
+#에라토스테네스의 체 알고리즘으로 해결 가능
 
 n = int(input())
 num = list(map(int, input().split()))
 result = 0
 
+# 2보다 작으면 소수가 아니므로 건너뜀
 for i in num:
     if i < 2:
         continue
-    
+# 소수 판별문
     is_prime = True
     for j in range(2, int(i**0.5) + 1):
         if(i % j == 0):
