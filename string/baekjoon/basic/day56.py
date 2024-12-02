847278f2b978a21f8959a889aea52553a4abed6f
DLongLegs <xoals73771@gmail.com>
2024-12-02 20:32:54 +0900

    Update day56.py

--- string/baekjoon/basic/day56.py
+++ string/baekjoon/basic/day56.py
@@ -1,9 +1,29 @@
-# 백준 25305번 문제
+# # 백준 25305번 문제
 
-N, k = map(int, input().split())
-socre = list(map(int, input().split()))
+# N, k = map(int, input().split())
+# socre = list(map(int, input().split()))
 
-socre.sort(reverse=True)
-print(socre)
-print(k)
-print(socre[k-1])
+# socre.sort(reverse=True)
+# print(socre[k-1])
+
+# 백준 2581번 문제
+
+M = int(input())
+N = int(input())
+result = []
+
+for i in range(M, N+1):
+    e=0
+    if i > 1:
+        for j in range(2, i):
+            if(i % j == 0):
+                e += 1
+                break
+        if e == 0:
+            result.append(i)
+
+if(len(result)>0):
+    print(sum(result))
+    print(min(result))
+else:
+    print(-1)
