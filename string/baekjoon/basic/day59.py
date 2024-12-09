869c432a022c68415fc5e98a991a5b8f5b72b3a5
DLongLegs <xoals73771@gmail.com>
2024-12-09 22:40:42 +0900

    Update day59.py

--- string/baekjoon/basic/day59.py
+++ string/baekjoon/basic/day59.py
@@ -1,5 +1,19 @@
-# 백준 24266번
+# # 백준 24266번
+
+# n = int(input())
+# print(n**3)
+# print(3)
+
+# 백준 24267번
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n - 2
+#         for j <- i + 1 to n - 1
+#             for k <- j + 1 to n
+#                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
+#     return sum;
+# }
 
 n = int(input())
-print(n**3)
+print(int(((n-2)*(n-1)*(2*n-3) + 3*(n-2)*(n-1)) / 12))
 print(3)
