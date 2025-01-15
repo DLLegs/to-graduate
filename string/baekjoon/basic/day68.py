ee4142c33b4e332e2039c7b29d75079d7730b33f
DLongLegs <xoals73771@gmail.com>
2025-01-15 15:29:09 +0900

    Create day68.py

---
+++ string/baekjoon/basic/day68.py
@@ -0,0 +1,38 @@
+# 백준 1181번 문제
+
+import sys
+input= sys.stdin.read
+data = input().splitlines()
+
+n = int(data[0])
+words =[]
+
+for i in range(1, n+1):
+    word = data[i]
+    words.append(word)
+
+words = list(set(words))
+words.sort(key=lambda x: (len(x), x))
+
+for word in words:
+    print(word)
+
+
+# 백준 10814번 문제
+
+import sys
+input = sys.stdin.read
+data = input().splitlines()
+
+N = int(data[0])
+humen = []
+
+for i in range(1, N+1):
+    age, name = data[i].split()
+    age = int(age)
+    humen.append((age, name))
+
+humen.sort(key=lambda rule: rule[0])
+
+for age, name in humen:
+    print(age,name)
