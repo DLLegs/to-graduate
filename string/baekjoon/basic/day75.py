64e9f6cc62654a8d811546037f2e4248d2f968f2
DLongLegs <xoals73771@gmail.com>
2025-02-02 12:25:56 +0900

    Create day75.py

---
+++ string/baekjoon/basic/day75.py
@@ -0,0 +1,24 @@
+# 백준 11487번 문제
+
+s = input().strip()
+
+substring = set()
+
+for i in range(len(s)):
+    for j in range(i+1, len(s)+1):
+        substring.add(s[i:j])
+
+print(len(substring))
+
+# 백준 1269번 복습
+import sys
+input = sys.stdin.read
+data = input().splitlines()
+n, m = map(int, data[0].split())
+
+A = set(map(int, data[1].split()))
+B = set(map(int, data[2].split()))
+
+union = A ^ B
+
+print(len(union))
