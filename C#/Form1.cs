61a3d645af9202176e0a96e21191d54febd11481
DLongLegs <100695795+DLLegs@users.noreply.github.com>
2025-03-06 22:44:00 +0900

    Add files via upload

---
+++ C#/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            /* 원폼에서 사용하는 모든 컨트롤에 대한 속성을 초기화 */
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "hello world";
+        }
+    }
+}
